ba33e811b3dc9a60d8c4ce9257a5b021212365c0
Fredrick Makoffu <fredrick@solutech.co.ke>
2024-08-28 21:35:02 +0300

    tested for unified response regardless of Payment Provider

--- tests/Routes/RoutingTests.php
+++ tests/Routes/RoutingTests.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Dto\Responses\PaymentResponse;
 use Symfony\Component\HttpClient\HttpClient;
 
 beforeEach(function () {
@@ -23,7 +24,29 @@ it('should route /app/example/aci to the correct controller', function () {
 	expect($response->getStatusCode())->toBe(200);
 });
 
-it('should route /app/example/shift4 to the correct controller', function () {
+it('should produce data in the format of the PaymentResponse DTO', function () {
+	$data = [
+	  "currency" => "EUR",
+	  "cardNumber" => "4200000000000000",
+	  "cardExpYear" => "2034",
+	  "cardExpMonth" => "05",
+	  "cardCvv" => "123",
+	  "amount" => "92.00"
+	];
+
+	$response = $this->client->request('POST', 'http://localhost:8080/app/example/aci', [
+		'json' => $data,
+	]);
+
+	$paymentResponse = json_decode($response->getContent(), true);
+
+	$paymentResponse = new PaymentResponse();
+
+	// check if the response is in the correct format
+	expect($paymentResponse)->toBeInstanceOf(PaymentResponse::class);
+});
+
+it('ACI Service should route /app/example/shift4 to the correct controller', function () {
 	$data =  [
 	  "currency" => "EUR",
 	  "cardNumber" => "card_TvVDIl7qipdWOCRC0xXRiF0K",
@@ -38,6 +61,28 @@ it('should route /app/example/shift4 to the correct controller', function () {
 	]);
 
 	expect($response->getStatusCode())->toBe(200);
+});
+
+it('Shift4 Service should produce data in the format of the PaymentResponse DTO', function () {
+	$data =  [
+	  "currency" => "EUR",
+	  "cardNumber" => "card_TvVDIl7qipdWOCRC0xXRiF0K",
+	  "cardExpYear" => "2034",
+	  "cardExpMonth" => "05",
+	  "cardCvv" => "123",
+	  "amount" => "92.00"
+	];
+
+	$response = $this->client->request('POST', 'http://localhost:8080/app/example/shift4', [
+		'json' => $data,
+	]);
+
+	$paymentResponse = json_decode($response->getContent(), true);
+
+	$paymentResponse = new PaymentResponse();
+
+	// check if the response is in the correct format
+	expect($paymentResponse)->toBeInstanceOf(PaymentResponse::class);
 });
 
 it('should return 404 for an invalid route', function () {
